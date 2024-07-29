e55f31cc782b8da7bd0d7939685091b5c4dbe6ea
KoodiApina <sulonkoodit@gmail.com>
2024-07-29 08:48:05 +0300

    Serial Reading test

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,25 @@
 import math
 import csv
+import datetime
+import serial
+import time
+
+def read_gps_data():
+    SERIAL_PORT = '/dev/ttyAMA0'  # or '/dev/ttyAMA0' depending on your Raspberry Pi model
+    BAUD_RATE = 9600
+    # Open the serial port
+    with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1) as ser:
+        while True:
+            # Read a line from the serial port
+            line = ser.readline().decode('ascii', errors='replace').strip()
+            
+            if line.startswith('$GNRMC'):  # NMEA sentence starting with $GNRMC
+                print(line)  # Print or process the line here
+            time.sleep(1)  # Adjust the sleep time as needed
 
 def runMain(camStatus) :
     csv_file_path = "/home/sulof/GPS/Python/GPS/CamLocation/cams.csv"
+    log_file_path = "/home/sulof/GPS/Python/GPS/Python/log.txt"
     
     with open(csv_file_path, mode='r') as file:
         csv_reader = csv.reader(file)
@@ -15,13 +32,14 @@ def runMain(camStatus) :
             place_coords = (25.278768, 60.628445) #Get coords from gps device!
             
             # Calculate distance
+            read_gps_data()
             distance = haversine_distance(camera_coords, place_coords)
             
             #Check if distance below 300m and then Alert!
             if (distance < 2) :
                 while (distance > 0.01) :
                     distance -= 0.0166 #Just for testing
-                    alert(distance)
+                    alert(distance, log_file_path)
                 camStatus = True
                 return camStatus
             
@@ -45,11 +63,10 @@ def haversine_distance(coord1, coord2):
     
     return distance
 
-def alert(distance) :
-    print(f">>>>>>>> {distance} <<<<<<<<")
-    #if (distance < 0.01) :
-    #    camStatus = False
-    #    return camStatus 
+def alert(distance, log_file_path) :
+    with open(log_file_path, 'a') as file:
+        file.write("Log entry: {}\n".format(datetime.datetime.now()))
+        file.write(f"{distance}\n")
 
 def camCheck(camStatus, distance) :
     if (camStatus == False) :
