34d2a95d7e82f9ef95cdb00bc15cde20185e9337
KoodiApina <sulonkoodit@gmail.com>
2024-07-31 01:00:24 +0300

    ReadCSV_C

--- C/main.c
+++ C/main.c
@@ -1,7 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void openFile(file_path) {
+	FILE *file = fopen(file_path, "r");
+	char buffer[100];
+	if (file == NULL) {
+		printf("Perkeleen virhe >:(");
+	}
+	else {
+		while (fgets(buffer, sizeof(buffer), file)) {
+			printf("%s", buffer);
+		}
+	}
+	fclose(file);
+}
+
 int main(void) {
-	printf("Hello, World!\n");
+	char file_path = "/home/sulof/GPS/CamLocation/cams.csv";
+	openFile(file_path);
 	return 0;
 }
