b804516b31d61cef11f992be0290e8266ed0a90c
SuloForsgren <sulo.forsgren@gmail.com>
2024-07-31 12:34:27 +0300

    CCCCCCC

--- C/main.c
+++ C/main.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>  // Include for strtok and strcspn
+#include <string.h>
 
+//Function for opening the cams.csv file
 void openFile(const char *file_path) {
     FILE *file = fopen(file_path, "r");
     char buffer[100];
@@ -19,14 +20,14 @@ void openFile(const char *file_path) {
         char *token = strtok(buffer, ",");
         if (token != NULL) {
             // Print the 1st column
-            printf("1st Column: %s\n", token);
+            printf("Longitude: %s\n", token);
 
             // Fetch and print the 2nd column
             token = strtok(NULL, ",");
             if (token != NULL) {
-                printf("2nd Column: %s\n", token);
+                printf("Latitude: %s\n", token);
             } else {
-                printf("No 2nd column!\n");
+                printf("No coords found\n");
             }
         } else {
             printf("No columns available\n");
@@ -34,11 +35,12 @@ void openFile(const char *file_path) {
     }
 
     fclose(file);
+    return;
 }
 
 int main(void) {
     const char *file_path = "/home/sulof/GPS/CamLocation/cams.csv";
-    openFile(file_path);
+    openFile(file_path)
     return 0;
 }
 
